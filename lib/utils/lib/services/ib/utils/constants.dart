0fe74b22bd85e4341c9046c7f8661e809282ee21
tpmsasu3b-gif <tpmsasu3b@gmail.com>
2026-05-11 10:12:54 +0700

    Create constants.dart

---
+++ lib/utils/lib/services/ib/utils/constants.dart
@@ -0,0 +1,25 @@
+class AppConstants {
+  static const String appName = 'First Aid Stock Manager';
+  static const String appVersion = '1.0.0';
+  
+  static const List<String> units = [
+    'Pcs',
+    'Roll',
+    'Box',
+    'Botol',
+    'Sachet',
+    'Lembar',
+    'Tube',
+    'Pasang',
+    'Tablet',
+    'Pack',
+  ];
+
+  static const Map<String, String> categoryEmojis = {
+    'perban': '🩹',
+    'obat': '💊',
+    'alat': '🩺',
+    'cairan': '💧',
+    'lainnya': '📦',
+  };
+}
